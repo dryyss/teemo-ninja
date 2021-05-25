07de2d1b0ff318739dbcffa6989e6601bbba10fe
ryadh <ryadh@imagia.com>
2021-05-24 20:46:29 -0400

    Update auto attack

--- src/Components/Player/index.jsx
+++ src/Components/Player/index.jsx
@@ -1,13 +1,44 @@
+/* eslint-disable no-constant-condition, arrow-body-style */
 import React, { useEffect, useMemo, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import { TimelineLite, Linear, gsap } from 'gsap';
 import _orderBy from 'lodash/orderBy';
+import _debounce from 'lodash/debounce';
 
 import { getWeapons } from '../../Store/selectors/Items';
 
 import './styles.scss';
 
 const tl = new TimelineLite({ repeat: -1, repeatDelay: 2 });
+
+const autoAttack = (refWeapon) => {
+  if (refWeapon.current) {
+    // step 1 : cooldown between each weapons
+    const weaponsGsap = gsap.utils.toArray('.player-weapon');
+    weaponsGsap.forEach((weaponGsap) => {
+      console.log('=> weaponGsap : ', weaponGsap);
+
+      tl.staggerFromTo(
+        [weaponGsap],
+        1,
+        {
+          backgroundImage: `url(./images/weapons/${weaponGsap.id.slice(
+            0,
+            7,
+          )}.png)`,
+          left: 70,
+          ease: Linear.easeNone,
+          opacity: 1,
+        },
+        {
+          left: 'calc(100% - 70px)',
+          delay: 0.5,
+        },
+        0.5,
+      ).staggerTo([weaponGsap], 0.5, { opacity: 0 }, 0.4);
+    });
+  }
+};
 
 const Player = () => {
   const refPlayer = useRef(null);
@@ -22,33 +53,10 @@ const Player = () => {
   });
 
   useEffect(() => {
-    if (refWeapon.current) {
-      // step 1 : cooldown between each weapons
-      const weaponsGsap = gsap.utils.toArray('.player-weapon');
-      weaponsGsap.forEach((weaponGsap) => {
-        tl.staggerFromTo(
-          [weaponGsap],
-          1,
-          {
-            backgroundImage: `url(./images/weapons/${weaponGsap.id.slice(
-              0,
-              7,
-            )}.png)`,
-            left: 70,
-            ease: Linear.easeNone,
-            opacity: 1,
-          },
-          {
-            left: 'calc(100% - 70px)',
-            delay: 1,
-          },
-          0.5,
-        ).staggerTo([weaponGsap], 0.5, { opacity: 0 }, 0.2);
-      });
-    }
-  }, [weapons]);
+    autoAttack(refWeapon);
 
-  const test = _orderBy(weapons, '', ['desc']);
+    return () => autoAttack({ current: null });
+  }, [refWeapon, weapons]);
 
   const playerWeapons = useMemo(
     () =>
